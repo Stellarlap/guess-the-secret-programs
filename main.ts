54d4ca7e29cbaa2e6d873ca882545348bf064310
Stellarlap <100299314+Stellarlap@users.noreply.github.com>
2022-04-13 10:32:39 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    random += 1
+    basic.showNumber(random)
+})
+input.onButtonPressed(Button.AB, function () {
+    if (true) {
+    	
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    random += -1
+    basic.showNumber(random)
+})
+let random = 0
+random = randint(0, 20)
+basic.showNumber(random)
